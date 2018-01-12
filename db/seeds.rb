865cc3a056fb3ae563132deeb368895a91be11f2
wuzhao <liwuzhao97@gmail.com>
2018-01-12 16:01:19 +0800

    sedd create an admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create_with(password: 'admin').find_or_create_by!(name: 'admin')
